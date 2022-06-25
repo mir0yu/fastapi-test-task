017cb1c8255cccc448228abbfe2b8ddb356c8b4e
mir0yu <peaceismir@gmail.com>
2022-06-25 19:11:48 +0300

    remove socket manager

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ from fastapi import (
 )
 from typing import List
 from fastapi.templating import Jinja2Templates
+import websockets
 
 app = FastAPI()
 
@@ -16,36 +17,36 @@ def get_chat(request: Request):
     return templates.TemplateResponse("chat.html", {"request": request})
 
 
-# Manager of connections
-class SocketManager:
-    def __init__(self):
-        self.active_connections: List[WebSocket] = []
+# # Manager of connections
+# class SocketManager:
+#     def __init__(self):
+#         self.active_connections: List[WebSocket] = []
 
-    async def connect(self, websocket: WebSocket):
-        await websocket.accept()
-        self.active_connections.append((websocket))
+#     async def connect(self, websocket: WebSocket):
+#         await websocket.accept()
+#         self.active_connections.append((websocket))
 
-    def disconnect(self, websocket: WebSocket):
-        self.active_connections.remove((websocket))
+#     def disconnect(self, websocket: WebSocket):
+#         self.active_connections.remove((websocket))
 
-    async def broadcast(self, websocket: WebSocket, data: dict):
-        if websocket in self.active_connections:
-            await websocket.send_json(data)
+#     async def broadcast(self, websocket: WebSocket, data: dict):
+#         if websocket in self.active_connections:
+#             await websocket.send_json(data)
 
 
-manager = SocketManager()
+# manager = SocketManager()
 
 # Subsribe on broadcast of Chat
 @app.websocket("/api/chat")
 async def chat(websocket: WebSocket):
         count = 0
-        await manager.connect(websocket)
+        await websocket.accept()
         try:
             while True:
                 data = await websocket.receive_json()
                 if (data):
                     count += 1
                     data.update({"count": count})
-                    await manager.broadcast(websocket, data)
+                    await websocket.send_json(data)
         except WebSocketDisconnect:
-            manager.disconnect(websocket)
+            websocket.close()
